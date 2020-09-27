078be26fcf0cdc01209dc6053a944c656391fc11
Tom <862343413@qq.com>
2020-09-27 22:23:11 +0800

    项目经理boss进行了修改

--- src/main/java/com/boss/controller/UserController.java
+++ src/main/java/com/boss/controller/UserController.java
@@ -24,6 +24,8 @@ import java.util.List;
 public class UserController {
     @GetMapping("/findAll")
     public String findAll() {
+        System.out.println("boss最厉害");
+
         User u1 = new User().setId("1").setName("tom");
         User u2 = new User().setId("2").setName("jack");
         User u3 = new User().setId("3").setName("rose");
